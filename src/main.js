4fd5ca398b358dd456512fbd89d6a8e2fc114ffa
soonwook34 <soonwook34@gmail.com>
2023-05-11 07:51:10 +0900

    main js file

---
+++ src/main.js
@@ -0,0 +1,71 @@
+// 데이터 관련
+let data, userNum, exerNum, conceptNum, models;
+// 시각화 관련
+let boxPlot, radarChart, heatmap;
+// 선택된 학생 ID, 문제 ID, 개념 ID, KT model
+let targetUser, targetExer, targetConcept, targetModel;
+
+let init = () => {
+    targetUser = 0;
+    targetExer = exerNum - 1;
+    targetConcept = 0;
+    targetModel = "GKT";
+
+    radarChart = new RadarChart("#radar-svg", data);
+    radarChart.init();
+    updateAll();
+};
+
+let updateRadarChart = () => {
+    radarChart.update(targetUser, targetExer, targetModel);
+};
+
+let updateAll = () => {
+    updateRadarChart();
+};
+
+d3.json("https://raw.githubusercontent.com/Soonwook34/KTvisualization/main/data/naive_c5_q50_s4000_v0.json")
+    .then(jsonData => {
+        // 데이터 가져오기
+        data = jsonData;
+        // 학생, 문제, 개념 수 구하기
+        userNum = data["user"].length;
+        exerNum = data["average_exer"].length;
+        conceptNum = data["average_exer"][0]["state_GKT"].length;
+        models = ["DKT", "GKT"];
+
+        console.log(data);
+        init();
+
+        // 학생 ID 선택 메뉴 추가
+        let userIdSelect = document.getElementById("user-id-select");
+        data["user"].forEach((_, idx) => {
+            let option = document.createElement("option");
+            option.innerHTML = idx + 1;
+            if (idx === targetUser) option.setAttribute("selected", "");
+            userIdSelect.appendChild(option);
+        });
+        // 학생 ID 선택 버튼 이벤트
+        d3.select("#user-id-btn").on("click", (e) => {
+            targetUser = userIdSelect.options[userIdSelect.selectedIndex].innerHTML - 1;
+            console.log(targetUser);
+            updateAll();
+            // TODO:
+        });
+
+        // KT model 선택 메뉴 추가
+        let modelSelect = document.getElementById("model-select");
+        models.forEach(model => {
+            let option = document.createElement("option");
+            option.innerHTML = model;
+            if (model === targetModel) option.setAttribute("selected", "");
+            modelSelect.appendChild(option);
+        });
+        // KT model 선택 메뉴 추가
+        d3.select("#model-btn").on("click", (e) => {
+            targetModel = modelSelect.options[modelSelect.selectedIndex].innerHTML
+            console.log(targetModel);
+            updateAll();
+            // TODO:
+        });
+    });
