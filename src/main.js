fb6235a7f2d32bac282c25bbbe2d54cb00c04a0c
soonwook34 <soonwook34@gmail.com>
2023-05-14 09:55:16 +0900

    add tooltip

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ let data, userNum, exerNum, conceptNum, models;
 let boxPlot, radarChart, heatmap;
 // 선택된 학생 ID, 문제 ID, 개념 ID, KT model
 let targetUser, targetExer, targetConcept, targetModel;
+let timer
 
 let init = () => {
     targetUser = 0;
@@ -17,7 +18,7 @@ let init = () => {
     radarChart = new RadarChart("#radar-svg", data);
     radarChart.init();
 
-    heatmap = new Heatmap("#heatmap-svg", data);
+    heatmap = new Heatmap("#heatmap-svg", "#heatmap-tooltip", data);
     heatmap.init();
 
     updateAll();
@@ -51,8 +52,20 @@ d3.json("https://raw.githubusercontent.com/Soonwook34/KTvisualization/main/data/
         conceptNum = data["average_exer"][0]["state_GKT"].length;
         models = ["DKT", "GKT"];
 
-        // console.log(data);
         init();
+        
+        // 자동 넘기기 스위치 이벤트
+        let tickTock = () => {
+            targetExer = (targetExer + 1) % exerNum;
+            updateAll();
+        };
+        let autoSwitch = document.getElementById("auto-switch");
+        autoSwitch.addEventListener("change", (_) => {
+            if (autoSwitch.checked) {
+                tickTock();
+                timer = setInterval(tickTock, 1000);
+            } else clearInterval(timer);
+        });
 
         // 학생 ID 선택 메뉴 추가
         let userIdSelect = document.getElementById("user-id-select");
@@ -66,9 +79,9 @@ d3.json("https://raw.githubusercontent.com/Soonwook34/KTvisualization/main/data/
         d3.select("#user-id-btn").on("click", (e) => {
             e.preventDefault();
             targetUser = userIdSelect.options[userIdSelect.selectedIndex].innerHTML - 1;
-            console.log(targetUser);
+            autoSwitch.checked = false;
+            clearInterval(timer);
             updateAll();
-            // TODO:
         });
 
         // KT model 선택 메뉴 추가
@@ -82,25 +95,47 @@ d3.json("https://raw.githubusercontent.com/Soonwook34/KTvisualization/main/data/
         // KT model 선택 메뉴 추가
         d3.select("#model-btn").on("click", (e) => {
             e.preventDefault();
-            targetModel = modelSelect.options[modelSelect.selectedIndex].innerHTML
-            console.log(targetModel);
+            targetModel = modelSelect.options[modelSelect.selectedIndex].innerHTML;
+            autoSwitch.checked = false;
+            clearInterval(timer);
             updateAll();
-            // TODO:
         });
         
-        heatmap.container.selectAll("rect.btn").on("click", (e) => {
+        // Heatmap click 이벤트
+        heatmap.container.selectAll("rect.btn").on("click", (e, d) => {
             e.preventDefault();
-            targetExer = e.target.__data__["exer"];
+            targetExer = d["exer"];
             updateAll();
         });
 
-        heatmap.container.selectAll("rect.btn").on("mouseover", (e) => {
+        // Heatmap mouseover 이벤트
+        heatmap.container.selectAll("rect.btn").on("mouseover", (e, d) => {
             e.preventDefault();
-            targetConcept = e.target.__data__["concept"].substr(1);
+            targetConcept = d["concept"]?.substr(1);
+
+            // Tooltip 처리
+            heatmap.tooltip.style("--bs-tooltip-bg", heatmap.conceptColorScale(d["concept"]));
+                
+            heatmap.tooltipInner.html(`${d["state"].toFixed(4)}`);
+            Popper.createPopper(e.target, heatmap.tooltip.node(), {
+                placement: "top",
+                modifiers: [{
+                    name: "arrow",
+                    options: {
+                        element: heatmap.tooltipArrow.node()
+                    }
+                }]
+            });
+            heatmap.tooltip.style("display", "block");
+            
             updateAll();
         });
-
+        // Heatmap mouseout 이벤트
         heatmap.container.selectAll("rect.btn").on("mouseout", (e) => {
+            e.preventDefault();
+            heatmap.tooltip.style("display", "none");
+        });
+        heatmap.container.selectAll("rect.background").on("mouseout", (e) => {
             e.preventDefault();
             targetConcept = -1;
             updateAll();
